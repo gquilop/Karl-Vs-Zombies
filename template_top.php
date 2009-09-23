b817ddb7ca0b3c10891ffd759d2a78c93ce7f106
Greg Quilop <keylope@gmail.com>
2009-09-23 00:59:08 -0400

    Added stylesheet

--- template_top.php
+++ template_top.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Humans vs. Zombies :: Source</title>
-	<link rel="stylesheet" type="text/css" href="styles.css" />
+	<link rel="stylesheet" type="text/css" href="style/styles.css" />
 	
 	<script type="text/javascript">
 		function goTo(page) {
